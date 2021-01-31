6e0fc1fa7d956b20b840faaa81c9ac9e8d2e714b
guoyixue <1010603372@qq.com>
2021-01-31 16:41:13 +0800

    测试代码提交

--- src/permission.js
+++ src/permission.js
@@ -6,7 +6,7 @@ router.beforeEach((to, from, next) => {
   let token = store.state.token.token
   if (token) {
     // 如果我当前是有权限的，手动输入地址到登录页面或者点击浏览器回退到登录页面，
-    //     那么不好意思，你就不能去登录页面（你就去跳转前的路径）
+    // 那么不好意思，你就不能去登录页面（你就去跳转前的路径）
     next()
   } else {
     next()
